13b323923e95aa58728b99c1386353525dba1f5c
Admin <Admin@DESKTOP-P5LSC6D>
2023-09-18 15:16:21 -0600

    Add project files.

---
+++ webApp.finalAssignment/wwwroot/js/site.js
@@ -0,0 +1,18 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+
+
+function animateSize() {
+    var headerText = document.getElementById("headerText");
+    headerText.classList.add('animated_text')
+}
+function defaultSize() {
+    var headerText = document.getElementById("headerText");
+    headerText.classList.remove('animated_text')
+}
+
+
+
